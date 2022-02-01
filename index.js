3f92d3b528b3bda17268ac32165a54377b4d04d0
Md Asifuzzaman Suvo <asifshuvo119181@gmail.com>
2022-02-02 04:16:50 +0600

    added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+var app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json())
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpassword@123',
+    database: 'employeesystem',
+    multipleStatements: true
+})
+
+app.post('/create', (req, res) => {
+    console.log(req.body)
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query(
+    'INSERT INTO employees (name, age, country, position,wage) VALUES(?,?,?,?,?)', 
+    [name, age, country, position, wage], 
+    (err, res) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send("Values inserted")
+        }
+    })
+
+})
+
+app.listen(3001, () => {
+    console.log("yey,your server is running on port 3001")
+})
